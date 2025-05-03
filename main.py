3ea6d3fb9883317bb908b396328d3579ba5b23aa
babapiro8484 <ytwozy7@gmail.com>
2025-05-03 17:30:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-import cpmelsedev import cpmelsedev
+import CPMelsedev import CPMelsedev
 
 def signal_handler(sig, frame):
     print("\n Bye Bye...")
